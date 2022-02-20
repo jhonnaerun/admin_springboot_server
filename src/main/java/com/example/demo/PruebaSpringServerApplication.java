8797c9ddf35e1ccb83abd4d82dcb025f5073c10e
jhonnatan <jhonnaerun@gmail.com>
2022-02-20 13:23:13 -0500

    Servidor springboot para prueba tecnica octans

---
+++ src/main/java/com/example/demo/PruebaSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringServerApplication.class, args);
+	}
+
+}
